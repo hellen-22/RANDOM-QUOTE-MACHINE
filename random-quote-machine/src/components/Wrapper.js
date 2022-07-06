79559a481f988b989d228a3c20ea3a98d6c10191
Hellen Wainaina <58620060+hellen-22@users.noreply.github.com>
2022-07-06 11:03:57 +0300

    creating react app

---
+++ random-quote-machine/src/components/Wrapper.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./../css/wrapper.css"
+
+function Wrapper() {
+  return (
+    <div className='wrapper'>
+        <h1>Wrapper</h1>
+    </div>
+  )
+}
+
+export default Wrapper
