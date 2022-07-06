8599ceb90365e0deed79111d01a5b0dde887a5c4
Hellen Wainaina <58620060+hellen-22@users.noreply.github.com>
2022-07-06 12:29:56 +0300

    wrapper element

--- random-quote-machine/src/components/Wrapper.js
+++ random-quote-machine/src/components/Wrapper.js
@@ -1,12 +1,38 @@
-import React from 'react'
-import "./../css/wrapper.css"
+import React from "react";
+import "./../css/wrapper.css";
 
 function Wrapper() {
   return (
-    <div className='wrapper'>
-        <h1>Wrapper</h1>
+    <div className="wrapper" id="quote-box">
+      <q id="text" className="quote-text">
+        The battles that count aren’t the ones for gold medals. The struggles
+        within yourself the invisible battles inside all of us that’s where it’s
+        at.
+      </q>
+      <p id="author" className="quote-author">
+        -Hellen Wainaina
+      </p>
+      <div className="buttons">
+        <div id="new-quote">
+          <button className="btn btn-primary">New Quote</button>
+        </div>
+        <div id="share-quote">
+          <a
+            className="sharebutton btn btn-primary"
+            href="twitter.com/intent/tweet"
+          >
+            Twitter
+          </a>
+          <a
+            className="sharebutton btn btn-primary"
+            href="twitter.com/intent/tweet"
+          >
+            LinkedIn
+          </a>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Wrapper
+export default Wrapper;
---
+++ random-quote-machine/src/screens/Body.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./../css/body.css"
+import Wrapper from '../components/Wrapper'
+
+function Body() {
+  return (
+    <div className='body'>
+        <Wrapper />
+    </div>
+  )
+}
+
+export default Body
