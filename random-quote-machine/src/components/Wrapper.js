e2df6ec57a470efab6010b98c3bc7dfe50817050
Hellen Wainaina <58620060+hellen-22@users.noreply.github.com>
2022-07-06 13:41:20 +0300

    change quote on button click

--- random-quote-machine/src/components/Wrapper.js
+++ random-quote-machine/src/components/Wrapper.js
@@ -1,33 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import "./../css/wrapper.css";
+import { FaTwitter, FaLinkedin } from "react-icons/fa";
 
 function Wrapper() {
+  const [quotes, setQuotes] = useState([
+    {
+      id: 1,
+      quote: "My name is Hellen and I am a software engineer.",
+      author: "Hellen",
+    },
+    {
+      id: 2,
+      quote: "I am a software engineer.",
+      author: "Wayne",
+    },
+    {
+      id: 3,
+      quote: "I am me.",
+      author: "Wairimu",
+    },
+  ]);
+
+  const [randomQuote, setRandomQuote] = useState(
+    quotes[Math.floor(Math.random() * quotes.length)]
+    );
+    
+
   return (
     <div className="wrapper" id="quote-box">
       <q id="text" className="quote-text">
-        The battles that count aren’t the ones for gold medals. The struggles
-        within yourself the invisible battles inside all of us that’s where it’s
-        at.
+        {randomQuote.quote}
       </q>
       <p id="author" className="quote-author">
-        -Hellen Wainaina
+        -{randomQuote.author}
       </p>
       <div className="buttons">
-        <div id="new-quote">
-          <button className="btn btn-primary">New Quote</button>
-        </div>
+        <button className="btn " onClick={() => setRandomQuote(quotes[Math.floor(Math.random() * quotes.length)])}>New Quote</button>
+
         <div id="share-quote">
-          <a
-            className="sharebutton btn btn-primary"
-            href="twitter.com/intent/tweet"
-          >
-            Twitter
+          <a className="sharebutton btn " href="twitter.com/intent/tweet">
+            <FaTwitter />
           </a>
-          <a
-            className="sharebutton btn btn-primary"
-            href="twitter.com/intent/tweet"
-          >
-            LinkedIn
+          <a className="sharebutton btn " href="twitter.com/intent/tweet">
+            <FaLinkedin />
           </a>
         </div>
       </div>
